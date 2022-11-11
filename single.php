cba5d83559fae001711057e804d65f2247435c99
Rebecca Cohen <rcohen2@ufl.edu>
2022-11-11 14:15:50 -0500

    single post created

--- single.php
+++ single.php
@@ -1,5 +1,24 @@
 <?php get_header(); ?>
 <main class="container">
+<?php
+    if(have_posts()){
+      while(have_posts()) {
+        the_post(); ?>
 
+        <div class="single-post">
+          <div class="featured-image">
+              <?php the_post_thumbnail(); ?>
+              <?php post_data(); ?>
+          </div><!--featured-image-->
+          <div class="text-container">
+            <h2><?php the_title();?></a></h2>
+            <p class="body-content"><?php the_content();?></p>
+            <?php dynamic_sidebar('sidebar-widget'); ?>
+          </div><!--text-container-->
+        </div><!--individual-post-->
+        <?php
+      }
+    }
+?>
   </main>
 <?php get_footer(); ?>

6ea4a754c1d5ed1329da7087c8037cfe01bbbd85
Rebecca Cohen <rcohen2@ufl.edu>
2022-11-11 14:15:22 -0500

    header & footer added

--- footer.php
+++ footer.php
@@ -1,5 +1,17 @@
-  <footer>
-    
+  <footer class="footer">
+    <div class="container">
+      <?php
+        dynamic_sidebar('top-footer-widget');
+        dynamic_sidebar('bottom-footer-widget');
+       ?>
+
+  <!-- Copyright -->
+  <div class="text-center p-4" >
+    © 2021 Copyright | Created by:
+    <a class="text-reset fw-bold" href="becreativedevelopment.com" target="_blank">Rebecca Cohen</a>
+  </div>
+  <!-- Copyright -->
+</div>
     </footer>
     <?php  wp_footer(); ?>
   </body>
--- header.php
+++ header.php
@@ -3,6 +3,10 @@
   <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700;800&family=Salsa&display=swap" rel="stylesheet">
 
     <title><?php bloginfo('name'); ?></title>
 
@@ -11,6 +15,30 @@
 
 <body <?php body_class()?>>
   <header>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 logo-container">
+          <?php
+              if(get_header_image() == ''){ ?>
+                  <h1><a href="<?php echo get_home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
+                <?php
+                }else{ ?>
+                  <a href="<?php echo get_home_url(); ?>"><img class="logo" src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="Company Logo" /></a>
+                <?php
+                  }
+              ?>
 
+        </div> <!--col-lg-6 logo-container -->
+
+        <div class="col-lg-6 navigation">
+          <nav class="custom-menu-class">
+            <?php
+              wp_nav_menu(array(
+                'theme_location' => 'main-nav'
+              ));
+             ?>
+           </nav>
+         </div> <!--/col-lg-6 navigation -->
+      </div> <!--/row-->
+    </div> <!--/container-->
   </header>
-</body>

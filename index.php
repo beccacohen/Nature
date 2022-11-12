cfc8ceabda6007557637272145ab06b466f03b5e
Rebecca Cohen <rcohen2@ufl.edu>
2022-11-12 18:46:41 -0500

    index thumbnail changed

--- index.php
+++ index.php
@@ -7,12 +7,11 @@
 
         <div class="individual-post">
           <div class="featured-image">
-              <?php the_post_thumbnail(); ?>
+              <?php the_post_thumbnail('thumbnail'); ?>
           </div><!--featured-image-->
           <div class="text-container">
-            <h2><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2>
+            <h2><a class="title-link"href="<?php the_permalink(); ?>"><?php the_title();?></a></h2>
             <p class="excerpt"><?php echo get_the_excerpt();?></p>
-            <?php dynamic_sidebar('sidebar-widget'); ?>
           </div><!--text-container-->
         </div><!--individual-post-->
         <?php

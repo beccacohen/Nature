7e6881ed4a8a919b489cc20d9d8ede40499fb8d1
Rebecca Cohen <rcohen2@ufl.edu>
2022-11-17 10:07:49 -0500

    404 page added

---
+++ 404.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+<div class="container">
+  <section class="page-404">
+    <div class="row">
+
+    <section class="error-container">
+      <span class="error">4</span>
+      <span class="error"><span class="screen-reader-text">0</span></span>
+      <span class="error">4</span>
+    </section>
+    <h1>Opps something went wrong</h1>
+    <div class="link-container">
+      <a href="<?php echo get_home_url(); ?>" class="more-link">Go home</a>
+    </div>
+  </div>
+</section>
+</div>
+
+<?php get_footer(); ?>

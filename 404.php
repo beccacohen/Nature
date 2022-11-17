941b2456f399919d30d0277edb70de9a87fb5dc3
Rebecca Cohen <rcohen2@ufl.edu>
2022-11-17 13:16:17 -0500

    added aside to 404

--- 404.php
+++ 404.php
@@ -3,17 +3,26 @@
   <section class="page-404">
     <div class="row">
 
-    <section class="error-container">
-      <span class="error">4</span>
-      <span class="error"><span class="screen-reader-text">0</span></span>
-      <span class="error">4</span>
-    </section>
-    <h1>Opps something went wrong</h1>
-    <div class="link-container">
-      <a href="<?php echo get_home_url(); ?>" class="more-link">Go home</a>
+      <div class="col-md-8 align-items-center">
+
+      <section class="error-container">
+        <span class="error">4</span>
+        <span class="error"><span class="screen-reader-text">0</span></span>
+        <span class="error">4</span>
+      </section>
+
+      <h1>Opps something went wrong</h1>
+      <div class="link-container">
+        <a href="<?php echo get_home_url(); ?>" class="more-link">Go home</a>
+      </div>
     </div>
-  </div>
-</section>
+
+      <aside class="col-md-3">
+
+        <?php dynamic_sidebar('sidebar-widget'); ?>
+      </aside>
+    </div>
+  </section>
 </div>
 
 <?php get_footer(); ?>
